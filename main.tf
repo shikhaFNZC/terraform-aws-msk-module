fa0390cea96cbc821211a6efd2fd1f8a37e1721f
Reuben Dunn <reuben.dunn@hypr.co.nz>
2020-04-14 16:42:31 +1200

    Updated and refactored main.tf to 0.12

--- main.tf
+++ main.tf
@@ -11,26 +11,13 @@ locals {
   count_client_authentication    = local.create_msk_cluster * (local.use_custom_configuration ? 0 : 1) * local.use_client_authentication
   count_msk_configuration        = length(var.msk_configuration_arn) > 0 ? 0 : local.create_custom_configuration
 
-  cluster_name        = var.cluster_name
   kafka_version       = var.kafka_version
-  num_of_broker_nodes = var.num_of_broker_nodes
-
-  broker_node_instance_type = var.broker_node_instance_type
-  broker_ebs_volume_size    = var.broker_ebs_volume_size
 
   client_subnets  = local.internal_vpc ? join(",", module.vpc.private_subnets) : join(",",var.client_subnets)
   security_groups = local.internal_vpc ? join(",", module.vpc.default_security_group) : join(",",var.security_groups)
 
   msk_configuration_arn      = length(var.msk_configuration_arn) > 0 ? var.msk_configuration_arn : element(concat(aws_msk_configuration.this.*.arn, list("")), 0)
   msk_configuration_revision = local.use_custom_configuration ? var.msk_configuration_revision : 1
-
-  client_broker_encryption = var.client_broker_encryption
-  in_cluster_encryption    = var.in_cluster_encryption
-  encryption_kms_key_arn   = var.encryption_kms_key_arn
-
-  certificate_authority_arns = var.certificate_authority_arns
-
-  enhanced_monitoring_level = var.enhanced_monitoring_level
 }
 
 module "vpc" {
@@ -48,103 +35,53 @@ module "vpc" {
   private_subnets = var.vpc_private_subnets
 }
 
-resource "aws_msk_cluster" "no_client_authentication" {
-  count                  = local.count_no_client_authentication
-  cluster_name           = local.cluster_name
+resource "aws_msk_cluster" "this" {
+  cluster_name           = var.cluster_name
   kafka_version          = local.kafka_version
-  number_of_broker_nodes = local.num_of_broker_nodes
+  number_of_broker_nodes = var.num_of_broker_nodes
 
   broker_node_group_info {
-    instance_type   = local.broker_node_instance_type
-    ebs_volume_size = local.broker_ebs_volume_size
+    instance_type   = var.broker_node_instance_type
+    ebs_volume_size = var.broker_ebs_volume_size
     client_subnets  = [split(",",local.client_subnets)]
     security_groups = [split(",",local.security_groups), aws_security_group.msk_cluster.id]
   }
 
   encryption_info {
     encryption_in_transit  {
-      client_broker = local.client_broker_encryption
-      in_cluster    = local.in_cluster_encryption
+      client_broker = var.client_broker_encryption
+      in_cluster    = var.in_cluster_encryption
     }
 
-    encryption_at_rest_kms_key_arn = local.encryption_kms_key_arn
+    encryption_at_rest_kms_key_arn = var.encryption_kms_key_arn
   }
 
-  enhanced_monitoring = local.enhanced_monitoring_level
+  enhanced_monitoring =  var.enhanced_monitoring_level
+
+
+  dynamic configuration_info {
+    for_each = local.count_custom_configuration
+      content {
+        arn      = local.msk_configuration_arn
+        revision = local.msk_configuration_revision
+      }
+  }
+
+
+  dynamic client_authentication {
+    for_each = local.count_client_authentication
+    content {
+      tls {
+        certificate_authority_arns = var.certificate_authority_arns
+      }
+    }
+  }
 
   tags = {
-    Name = local.cluster_name
+    Name = var.cluster_name
   }
 }
 
-resource "aws_msk_cluster" "custom_configuration" {
-  count                  = local.count_custom_configuration
-  cluster_name           = local.cluster_name
-  kafka_version          = local.kafka_version
-  number_of_broker_nodes = local.num_of_broker_nodes
-
-  broker_node_group_info {
-    instance_type   = local.broker_node_instance_type
-    ebs_volume_size = local.broker_ebs_volume_size
-    client_subnets  = [split(",",local.client_subnets)]
-    security_groups = [split(",",local.security_groups), aws_security_group.msk_cluster.id]
-  }
-
-  configuration_info {
-    arn      = local.msk_configuration_arn
-    revision = local.msk_configuration_revision
-  }
-
-  encryption_info {
-    encryption_in_transit  {
-      client_broker = local.client_broker_encryption
-      in_cluster    = local.in_cluster_encryption
-    }
-
-    encryption_at_rest_kms_key_arn = local.encryption_kms_key_arn
-  }
-
-  enhanced_monitoring = local.enhanced_monitoring_level
-
-  tags = {
-    Name = local.cluster_name
-  }
-}
-
-resource "aws_msk_cluster" "client_authentication" {
-  count                  = local.count_client_authentication
-  cluster_name           = local.cluster_name
-  kafka_version          = local.kafka_version
-  number_of_broker_nodes = local.num_of_broker_nodes
-
-  broker_node_group_info {
-    instance_type   = local.broker_node_instance_type
-    ebs_volume_size = local.broker_ebs_volume_size
-    client_subnets  = [split(",",local.client_subnets)]
-    security_groups = [split(",",local.security_groups), aws_security_group.msk_cluster.id]
-  }
-
-  client_authentication {
-    tls {
-      certificate_authority_arns = local.certificate_authority_arns
-    }
-  }
-
-  encryption_info {
-    encryption_in_transit  {
-      client_broker = local.client_broker_encryption
-      in_cluster    = local.in_cluster_encryption
-    }
-
-    encryption_at_rest_kms_key_arn = local.encryption_kms_key_arn
-  }
-
-  enhanced_monitoring = local.enhanced_monitoring_level
-
-  tags = {
-    Name = local.cluster_name
-  }
-}
 
 resource "aws_msk_configuration" "this" {
   count          = local.count_msk_configuration
